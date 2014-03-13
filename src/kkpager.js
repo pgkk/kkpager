939d4049b2d3df8134696b7355f9b0c0376a9a11
tom <cqzhangkang@163.com>
2014-03-13 19:36:42 +0800

    update to v1.2

--- src/kkpager.js
+++ src/kkpager.js
@@ -1,47 +1,49 @@
 ﻿/*
-  A JavaScript implementation of kkpager paging display controls
+  一个分页展示按钮控件,kkpager V1.2
   https://github.com/pgkk/kkpager
 
   Copyright (c) 2013 cqzhangkang@163.com
   Licensed under the GNU GENERAL PUBLIC LICENSE
 */
 var kkpager = {
-		//divID
-		pagerid : 'kkpager',
-		//当前页码
-		pno : 1,
-		//总页码
-		total : 1,
-		//总数据条数
-		totalRecords : 0,
-		//是否显示总页数
-		isShowTotalPage : true,
-		//是否显示总记录数
-		isShowTotalRecords : true,
-		//是否显示页码跳转输入框
-		isGoPage : true,
-		//链接前部
-		hrefFormer : '',
-		//链接尾部
-		hrefLatter : '',
-		lang : {
-			prePageText : '上一页',
-			nextPageText : '下一页',
-			totalPageBeforeText : '共',
-			totalPageAfterText : '页',
-			totalRecordsAfterText : '条数据',
-			gopageBeforeText : '转到',
-			gopageButtonOkText : '确定',
-			gopageAfterText : '页',
-			buttonTipBeforeText : '第',
-			buttonTipAfterText : '页'
+		pagerid 			: 'kkpager', //divID
+		mode				: 'link', //模式(link 或者 click)
+		pno					: 1, //当前页码
+		total				: 1, //总页码
+		totalRecords		: 0, //总数据条数
+		isShowFirstPageBtn	: true, //是否显示首页按钮
+		isShowLastPageBtn	: true, //是否显示尾页按钮
+		isShowPrePageBtn	: true, //是否显示上一页按钮
+		isShowNextPageBtn	: true, //是否显示下一页按钮
+		isShowTotalPage 	: true, //是否显示总页数
+		isShowTotalRecords 	: true, //是否显示总记录数
+		isGoPage 			: true,	//是否显示页码跳转输入框
+		hrefFormer			: '', //链接前部
+		hrefLatter			: '', //链接尾部
+		gopageWrapId		: 'kkpager_gopage_wrap',
+		gopageButtonId		: 'kkpager_btn_go',
+		gopageTextboxId		: 'kkpager_btn_go_input',
+		lang				: {
+			firstPageText			: '首页',
+			firstPageTipText		: '首页',
+			lastPageText			: '尾页',
+			lastPageTipText			: '尾页',
+			prePageText				: '上一页',
+			prePageTipText			: '上一页',
+			nextPageText			: '下一页',
+			nextPageTipText			: '下一页',
+			totalPageBeforeText		: '共',
+			totalPageAfterText		: '页',
+			totalRecordsAfterText	: '条数据',
+			gopageBeforeText		: '转到',
+			gopageButtonOkText		: '确定',
+			gopageAfterText			: '页',
+			buttonTipBeforeText		: '第',
+			buttonTipAfterText		: '页'
 		},
-		gopageWrapId : 'kkpager_gopage_wrap',
-		gopageButtonId : 'kkpager_btn_go',
-		gopageTextboxId : 'kkpager_btn_go_input',
 		
-		/****链接算法****/
-		getLink : function(n){
+		//链接算法（当处于link模式）,参数n为页码
+		getLink	: function(n){
 			//这里的算法适用于比如：
 			//hrefFormer=http://www.xx.com/news/20131212
 			//hrefLatter=.html
@@ -53,6 +55,18 @@ var kkpager = {
 			}else{
 				return this.hrefFormer + '_' + n + this.hrefLatter;
 			}
+		},
+		
+		//页码单击事件处理函数（当处于mode模式）,参数n为页码
+		click	: function(n){
+			//这里自己实现
+			//这里可以用this或者kkpager访问kkpager对象
+			return false;
+		},
+		//获取href的值（当处于mode模式）,参数n为页码
+		getHref	: function(n){
+			//默认返回'#'
+			return '#';
 		},
 		//跳转框得到输入焦点时
 		focus_gopage : function (){
@@ -87,73 +101,67 @@ var kkpager = {
 				return;
 			}
 			var n = parseInt(str_page);
-			if(n < 1 || n >this.total){
-				$('#'+this.gopageTextboxId).val(this.next);
-				return;
+			if(n < 1) n = 1;
+			if(n > this.total) n = this.total;
+			if(this.mode == 'click'){
+				this._clickHandler(n);
+			}else{
+				window.location = this.getLink(n);
 			}
-			//这里可以按需改window.open
-			window.location = this.getLink(n);
 		},
-		//分页按钮控件初始化
-		init : function(config){
-			this.pno = isNaN(config.pno) ? 1 : parseInt(config.pno);
-			this.total = isNaN(config.total) ? 1 : parseInt(config.total);
-			this.totalRecords = isNaN(config.totalRecords) ? 0 : parseInt(config.totalRecords);
-			if(config.pagerid){this.pagerid = config.pagerid;}
-			if(config.gopageWrapId){this.gopageWrapId = config.gopageWrapId;}
-			if(config.gopageButtonId){this.gopageButtonId = config.gopageButtonId;}
-			if(config.gopageTextboxId){this.gopageTextboxId = config.gopageTextboxId;}
-			if(config.isShowTotalPage != undefined){this.isShowTotalPage=config.isShowTotalPage;}
-			if(config.isShowTotalRecords != undefined){this.isShowTotalRecords=config.isShowTotalRecords;}
-			if(config.isGoPage != undefined){this.isGoPage=config.isGoPage;}
-			if(config.lang){
-				for(var key in config.lang){
-					this.lang[key] = config.lang[key];
-				}
-			}
-			this.hrefFormer = config.hrefFormer || '';
-			this.hrefLatter = config.hrefLatter || '';
-			if(config.getLink && typeof(config.getLink) == 'function'){this.getLink = config.getLink;}
-			//validate
-			if(this.pno < 1) this.pno = 1;
-			this.total = (this.total <= 1) ? 1: this.total;
-			if(this.pno > this.total) this.pno = this.total;
-			this.prv = (this.pno<=2) ? 1 : (this.pno-1);
-			this.next = (this.pno >= this.total-1) ? this.total : (this.pno + 1);
-			this.hasPrv = (this.pno > 1);
-			this.hasNext = (this.pno < this.total);
-			
-			this.inited = true;
+		//不刷新页面直接手动调用选中某一页码
+		selectPage : function(n){
+			this._config['pno'] = n;
+			this.generPageHtml(this._config,true);
 		},
 		//生成控件代码
-		generPageHtml : function(){
-			if(!this.inited){
-				return;
+		generPageHtml : function(config,enforceInit){
+			if(enforceInit || !this.inited){
+				this.init(config);
 			}
 			
-			var str_prv='',str_next='';
-			if(this.hasPrv){
-				str_prv = '<a href="'+this.getLink(this.prv)+'" title="'
-					+this.lang.prePageText+'">'+this.lang.prePageText+'</a>';
-			}else{
-				str_prv = '<span class="disabled">'+this.lang.prePageText+'</span>';
+			var str_first='',str_prv='',str_next='',str_last='';
+			if(this.isShowFirstPageBtn){
+				if(this.hasPrv){
+					str_first = '<a '+this._getHandlerStr(1)+' title="'
+						+(this.lang.firstPageTipText || this.lang.firstPageText)+'">'+this.lang.firstPageText+'</a>';
+				}else{
+					str_first = '<span class="disabled">'+this.lang.firstPageText+'</span>';
+				}
 			}
-			
-			if(this.hasNext){
-				str_next = '<a href="'+this.getLink(this.next)+'" title="'
-					+this.lang.nextPageText+'">'+this.lang.nextPageText+'</a>';
-			}else{
-				str_next = '<span class="disabled">'+this.lang.nextPageText+'</span>';
+			if(this.isShowPrePageBtn){
+				if(this.hasPrv){
+					str_prv = '<a '+this._getHandlerStr(this.prv)+' title="'
+						+(this.lang.prePageTipText || this.lang.prePageText)+'">'+this.lang.prePageText+'</a>';
+				}else{
+					str_prv = '<span class="disabled">'+this.lang.prePageText+'</span>';
+				}
+			}
+			if(this.isShowNextPageBtn){
+				if(this.hasNext){
+					str_next = '<a '+this._getHandlerStr(this.next)+' title="'
+						+(this.lang.nextPageTipText || this.lang.nextPageText)+'">'+this.lang.nextPageText+'</a>';
+				}else{
+					str_next = '<span class="disabled">'+this.lang.nextPageText+'</span>';
+				}
+			}
+			if(this.isShowLastPageBtn){
+				if(this.hasNext){
+					str_last = '<a '+this._getHandlerStr(this.total)+' title="'
+						+(this.lang.lastPageTipText || this.lang.lastPageText)+'">'+this.lang.lastPageText+'</a>';
+				}else{
+					str_last = '<span class="disabled">'+this.lang.lastPageText+'</span>';
+				}
 			}
 			var str = '';
 			var dot = '<span>...</span>';
 			var total_info='';
 			if(this.isShowTotalPage || this.isShowTotalRecords){
-				total_info = '<span class="normalsize">'+this.lang.totalPageBeforeText;
+				total_info = '&nbsp;<span class="normalsize">'+this.lang.totalPageBeforeText;
 				if(this.isShowTotalPage){
 					total_info += this.total + this.lang.totalPageAfterText;
 					if(this.isShowTotalRecords){
-						total_info += '&nbsp;/&nbsp;';
+						total_info += '/';
 					}
 				}
 				if(this.isShowTotalRecords){
@@ -177,7 +185,7 @@ var kkpager = {
 					if(this.pno == i){
 						str += '<span class="curr">'+i+'</span>';
 					}else{
-						str += '<a href="'+this.getLink(i)+'" title="'
+						str += '<a '+this._getHandlerStr(i)+' title="'
 							+this.lang.buttonTipBeforeText + i + this.lang.buttonTipAfterText+'">'+i+'</a>';
 					}
 				}
@@ -187,15 +195,15 @@ var kkpager = {
 						if(this.pno == i){
 							str += '<span class="curr">'+i+'</span>';
 						}else{
-							str += '<a href="'+this.getLink(i)+'" title="'+
+							str += '<a '+this._getHandlerStr(i)+' title="'+
 								this.lang.buttonTipBeforeText + i + this.lang.buttonTipAfterText+'">'+i+'</a>';
 						}
 					}
 					str += dot;
 				}else{
-					str += '<a href="'+this.getLink(1)+'" title="'
+					str += '<a '+this._getHandlerStr(1)+' title="'
 						+this.lang.buttonTipBeforeText + '1' + this.lang.buttonTipAfterText+'">1</a>';
-					str += '<a href="'+this.getLink(2)+'" title="'
+					str += '<a '+this._getHandlerStr(2)+' title="'
 						+this.lang.buttonTipBeforeText + '2' + this.lang.buttonTipAfterText +'">2</a>';
 					str += dot;
 					
@@ -214,7 +222,7 @@ var kkpager = {
 						if(this.pno == i){
 							str += '<span class="curr">'+i+'</span>';
 						}else{
-							str += '<a href="'+this.getLink(i)+'" title="'
+							str += '<a '+this._getHandlerStr(i)+' title="'
 								+this.lang.buttonTipBeforeText + i + this.lang.buttonTipAfterText+'">'+i+'</a>';
 						}
 					}
@@ -224,7 +232,62 @@ var kkpager = {
 				}
 			}
 			
-			str = "&nbsp;"+str_prv + str + str_next  + total_info + gopage_info;
+			str = "&nbsp;" + str_first + str_prv + str + str_next + str_last + total_info + gopage_info;
 			$("#"+this.pagerid).html(str);
+		},
+		//分页按钮控件初始化
+		init : function(config){
+			this.pno = isNaN(config.pno) ? 1 : parseInt(config.pno);
+			this.total = isNaN(config.total) ? 1 : parseInt(config.total);
+			this.totalRecords = isNaN(config.totalRecords) ? 0 : parseInt(config.totalRecords);
+			if(config.pagerid){this.pagerid = config.pagerid;}
+			if(config.mode){this.mode = config.mode;}
+			if(config.gopageWrapId){this.gopageWrapId = config.gopageWrapId;}
+			if(config.gopageButtonId){this.gopageButtonId = config.gopageButtonId;}
+			if(config.gopageTextboxId){this.gopageTextboxId = config.gopageTextboxId;}
+			if(config.isShowFirstPageBtn != undefined){this.isShowFirstPageBtn=config.isShowFirstPageBtn;}
+			if(config.isShowLastPageBtn != undefined){this.isShowLastPageBtn=config.isShowLastPageBtn;}
+			if(config.isShowPrePageBtn != undefined){this.isShowPrePageBtn=config.isShowPrePageBtn;}
+			if(config.isShowNextPageBtn != undefined){this.isShowNextPageBtn=config.isShowNextPageBtn;}
+			if(config.isShowTotalPage != undefined){this.isShowTotalPage=config.isShowTotalPage;}
+			if(config.isShowTotalRecords != undefined){this.isShowTotalRecords=config.isShowTotalRecords;}
+			if(config.isGoPage != undefined){this.isGoPage=config.isGoPage;}
+			if(config.lang){
+				for(var key in config.lang){
+					this.lang[key] = config.lang[key];
+				}
+			}
+			this.hrefFormer = config.hrefFormer || '';
+			this.hrefLatter = config.hrefLatter || '';
+			if(config.getLink && typeof(config.getLink) == 'function'){this.getLink = config.getLink;}
+			if(config.click && typeof(config.click) == 'function'){this.click = config.click;}
+			if(config.getHref && typeof(config.getHref) == 'function'){this.getHref = config.getHref;}
+			if(!this._config){
+				this._config = config;
+			}
+			//validate
+			if(this.pno < 1) this.pno = 1;
+			this.total = (this.total <= 1) ? 1: this.total;
+			if(this.pno > this.total) this.pno = this.total;
+			this.prv = (this.pno<=2) ? 1 : (this.pno-1);
+			this.next = (this.pno >= this.total-1) ? this.total : (this.pno + 1);
+			this.hasPrv = (this.pno > 1);
+			this.hasNext = (this.pno < this.total);
+			
+			this.inited = true;
+		},
+		_getHandlerStr : function(n){
+			if(this.mode == 'click'){
+				return 'href="'+this.getHref(n)+'" onclick="return kkpager._clickHandler('+n+')"';
+			}
+			//link模式，也是默认的
+			return 'href="'+this.getLink(n)+'"';
+		},
+		_clickHandler	: function(n){
+			var res = false;
+			if(this.click && typeof this.click == 'function'){
+				res = this.click.call(this,n) || false;
+			}
+			return res;
 		}
 };
